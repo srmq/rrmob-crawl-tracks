978d8d62df284b77d13d533c272e3aa81f025724
srmq <srmq@cin.ufpe.br>
2020-04-10 18:21:46 -0300

    dbclasses workinprogress

--- app/dbclasses.py
+++ app/dbclasses.py
@@ -14,15 +14,20 @@ class User(Base):
 
     user_obj = Column(JSONB)
     spot_id = Column(String(1024), unique=True, index=True)    
-    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
+    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)
+    #relationship
+    playHistory = relationship("PlayHistory", back_populates="parentUser")    
 
 class FullTrack(Base):
     __tablename__ = 'tracks'
     id = Column('id', Integer, primary_key=True)
     fulltrack_obj = Column(JSONB, nullable=False)
     spot_id = Column(String(1024), unique=True, index=True, nullable=False)        
-    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
-
+    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)
+    #relationships
+    played = relationship("PlayHistory", back_populates="fullTrack")
+    audioFeatures = relationship("AudioFeatures", back_populates="parentTrack", uselist=False)
+    audioAnalysis = relationship("AudioAnalysis", back_populates="parentTrack", uselist=False)
 
 class PlayHistory(Base):
     __tablename__ = 'play_history'
@@ -37,6 +42,12 @@ class PlayHistory(Base):
     from_playlist = Column(Integer, ForeignKey('playlists.id'))
     from_album = Column(Integer, ForeignKey('albuns.id'))
     retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)
+    #relationships
+    parentUser = relationship("User", back_populates="playHistory")
+    fullTrack = relationship("FullTrack", back_populates="played")
+    fromAlbum = relationship("FullAlbum", back_populates="played")
+    fromPlaylist = relationship("FullPlaylist", back_populates="played")
+    fromArtist = relationship("FullArtist", back_populates="played")    
 
 class AudioFeatures(Base):
     __tablename__ = 'audio_features'    
@@ -44,7 +55,9 @@ class AudioFeatures(Base):
     id = Column('id', Integer, primary_key=True)
     feature_obj = Column(JSONB, nullable=False)
     fulltrack_id = Column(Integer, ForeignKey('tracks.id'), nullable=False, unique=True)
-    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
+    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)
+
+    parentTrack = relationship("FullTrack", back_populates="audioFeatures")        
 
 class AudioAnalysis(Base):
     __tablename__ = 'audio_analysis'    
@@ -52,7 +65,9 @@ class AudioAnalysis(Base):
     id = Column('id', Integer, primary_key=True)
     analysis_obj = Column(JSONB, nullable=False)
     fulltrack_id = Column(Integer, ForeignKey('tracks.id'), nullable=False, unique=True)
-    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
+    retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)  
+
+    parentTrack = relationship("FullTrack", back_populates="audioAnalysis")      
 
 class FullArtist(Base):
     __tablename__ = 'artists'
@@ -61,6 +76,8 @@ class FullArtist(Base):
     spot_id = Column(String(1024), unique=True, index=True, nullable=False)
     retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
 
+    played = relationship("PlayHistory", back_populates="fromArtist")    
+
 class FullPlaylist(Base):
     __tablename__ = 'playlists'
     id = Column('id', Integer, primary_key=True)
@@ -68,9 +85,13 @@ class FullPlaylist(Base):
     spot_id = Column(String(1024), unique=True, index=True, nullable=False)
     retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
 
+    played = relationship("PlayHistory", back_populates="fromPlaylist")    
+
 class FullAlbum(Base):
     __tablename__ = 'albuns'
     id = Column('id', Integer, primary_key=True)
     fullalbum_obj = Column(JSONB, nullable=False)
     spot_id = Column(String(1024), unique=True, index=True, nullable=False)
     retrieved_at = Column(DateTime, server_default=func.now(), nullable=False)    
+
+    played = relationship("PlayHistory", back_populates="fromAlbum")
